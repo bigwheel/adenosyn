26bee050748bc457a0cad54ccab8b996b12e90d0
bigwheel <k.bigwheel+eng@gmail.com>
2016-03-06 01:35:25 +0900

    helloworldのテストを追加

--- src/test/scala/com/github/bigwheel/youseibox/MainSpec.scala
+++ src/test/scala/com/github/bigwheel/youseibox/MainSpec.scala
@@ -1,9 +1,17 @@
 package com.github.bigwheel.youseibox
 
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 import org.scalatest._
 
 class MainSpec extends FunSpec with Matchers {
   it("Main.mainを呼べる") {
-    Main.main(Array[String]())
+    val outStream = new ByteArrayOutputStream
+    val out = new PrintStream(outStream, true, "utf-8")
+    Console.withOut(out) {
+      Main.main(Array[String]())
+      out.flush()
+      outStream.toString("utf-8") shouldEqual "hello\n"
+    }
   }
 }
