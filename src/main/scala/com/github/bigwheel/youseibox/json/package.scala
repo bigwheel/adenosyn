a97fc7c2d2f9861857f2a3a5d0c947572fdb7137
bigwheel <k.bigwheel+eng@gmail.com>
2016-04-03 13:49:27 +0900

    既存のコードを削除

--- src/main/scala/com/github/bigwheel/youseibox/json/package.scala
+++ src/main/scala/com/github/bigwheel/youseibox/json/package.scala
@@ -1,92 +1,15 @@
 package com.github.bigwheel.youseibox
 
-import com.github.bigwheel.youseibox.table._
-import scala.util.Random
-import scalaz.Scalaz._
-
 package object json {
 
-  trait JsValue {
-    def toSql: SqlFragment
-  }
+  trait JsValue
 
-  case class JsString(val tableName: String, val columnName: String) extends JsValue {
-    def toSql = SqlFragment(s""" '"', $tableName.$columnName, '"' """)
-  }
+  abstract class JsString extends JsValue
 
-  case class JsInt(val tableName: String, val columnName: String) extends JsValue {
-    def toSql = SqlFragment(tableName + "." + columnName)
-  }
+  abstract class JsInt extends JsValue
 
-  case class JsArray(
-    td: Option[_1toNTable],
-    value: JsValue
-  ) extends JsValue {
-    lazy val toSql = {
-      td match {
-        case Some(tableDefinition) =>
-          val temporaryViewName = Random.alphanumeric.take(10).mkString
-          val preProcess = s"""
-            |CREATE VIEW $temporaryViewName AS
-            |SELECT
-            |  ${tableDefinition.name}.${tableDefinition.joinColumnName},
-            |  CONCAT('[', GROUP_CONCAT(${value.toSql.selectMain} SEPARATOR ','), ']') AS names
-            |FROM ${tableDefinition.name}
-            |${value.toSql.joinFragment.getOrElse("")}
-            |GROUP BY ${tableDefinition.name}.${tableDefinition.joinColumnName}
-            """.stripMargin
-          val postProcess = s"DROP VIEW if exists $temporaryViewName"
-          SqlFragment(
-            value.toSql.preProcess :+ preProcess,
-            s"$temporaryViewName.names",
-            s"""
-               |JOIN
-               |  $temporaryViewName
-               |ON
-               |  ${tableDefinition.parentColumnName} = $temporaryViewName.${tableDefinition.joinColumnName}
-               |""".stripMargin.some,
-            postProcess +: value.toSql.postProcess
-          )
-        case None =>
-        // TODO: Noneの場合のコードも書け
-          throw new RuntimeException()
-      }
-    }
-  }
+  abstract class JsArray extends JsValue
 
-  case class JsObject(
-    tdo: Option[Table],
-    properties: Map[String, JsValue]
-  ) extends JsValue {
-    def toSql = {
-      val jsonObjectColumn = {
-        val ps = properties.map { case (k, v) =>
-          s""" '"$k":', ${v.toSql.selectMain}, """
-        }.mkString("',',\n")
-        s"CONCAT( '{', $ps '}' ) "
-      }
-      val joinFragments = properties.values.flatMap(_.toSql.joinFragment).mkString("\n")
-
-      val (selectMain, joinFragment) = tdo match {
-        case Some(tableDefinition: RootTable) =>
-          val sm = s"SELECT $jsonObjectColumn AS json FROM ${tableDefinition.name} " +
-            tableDefinition.getJoinStrings + joinFragments
-          (sm, None)
-        case Some(_) =>
-          throw new IllegalStateException("")
-        case None =>
-          // TODO ここscalazの元の概念(モナドかも？)を使えばこんな無様なコードにはならない
-          (jsonObjectColumn, joinFragments.some)
-      }
-
-      SqlFragment(
-        properties.values.flatMap(_.toSql.preProcess).toSeq,
-        selectMain,
-        joinFragment,
-        properties.values.flatMap(_.toSql.postProcess).toSeq
-      )
-
-    }
-  }
+  abstract class JsObject extends JsValue
 
 }
--- src/main/scala/com/github/bigwheel/youseibox/table/package.scala
+++ src/main/scala/com/github/bigwheel/youseibox/table/package.scala
@@ -2,45 +2,6 @@ package com.github.bigwheel.youseibox
 
 package object table {
 
-  trait Table {
-    val name: String
-    protected[this] val chainTable: Option[_1to1Table]
-    def getJoinStrings: String = chainTable.map(_.joinStrings).getOrElse("")
-  }
-
-  object RootTable {
-    def apply(name: String, chainTable: Option[_1to1Table] = None) = new RootTable(name, chainTable)
-  }
-
-  class RootTable(
-    val name: String,
-    protected[this] val chainTable: Option[_1to1Table] = None
-  ) extends Table
-
-  object _1to1Table {
-    def apply(name: String, joinRule: String, chainTable: Option[_1to1Table] = None): _1to1Table =
-      new _1to1Table(name, joinRule, chainTable)
-  }
-
-  class _1to1Table(
-    val name: String,
-    joinRule: String,
-    protected[this] val chainTable: Option[_1to1Table] = None
-  ) extends Table {
-    val joinStrings: String = s"JOIN $name ON $joinRule\n" + chainTable.map(_.joinStrings).getOrElse("")
-  }
-
-  object _1toNTable {
-    def apply(name: String, joinColumnName: String, parentColumnName: String,
-      chainTable: Option[_1to1Table] = None) =
-      new _1toNTable(name, joinColumnName, parentColumnName, chainTable)
-  }
-
-  class _1toNTable(
-    val name: String,
-    val joinColumnName: String,
-    val parentColumnName: String,
-    protected[this] val chainTable: Option[_1to1Table] = None
-  ) extends Table
+  class Table(val name: String)
 
 }
