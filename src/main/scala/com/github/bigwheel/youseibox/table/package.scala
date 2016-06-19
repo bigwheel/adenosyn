63c006ae5f9269ed84d16a16f44c8f91f5d10ddc
bigwheel <k.bigwheel+eng@gmail.com>
2016-06-19 23:07:26 +0900

    不要なコードを削除

--- src/main/scala/com/github/bigwheel/youseibox/table/package.scala
+++ src/main/scala/com/github/bigwheel/youseibox/table/package.scala
@@ -5,14 +5,6 @@ import Scalaz._
 
 package object table {
 
-  case class OldTable(name: String, chainedTableOption: Option[ChainedTable] = None) {
-    def definition = chainedTableOption match {
-      case Some(ct) => s"$name JOIN ${ct.name} ON" +
-        s" $name.${ct.columnOfParentTable} = ${ct.name}.${ct.columnOfChainedTable}"
-      case None => name
-    }
-  }
-
   case class ChainedTable(name: String, columnOfParentTable: String, columnOfChainedTable: String)
   case class _1toNTable(name: String, columnOfParentTable: String, columnOfChainedTable: String)
 
