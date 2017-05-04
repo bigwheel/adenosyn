c32d9e4371928679518f327375147643cdd0dfde
bigwheel <k.bigwheel+eng@gmail.com>
2017-05-04 13:08:55 +0900

    Separate query generation and execution

--- changerecorder/src/main/scala/com/github/bigwheel/adenosyn/changerecorder/ChangeRecorder.scala
+++ changerecorder/src/main/scala/com/github/bigwheel/adenosyn/changerecorder/ChangeRecorder.scala
@@ -17,56 +17,74 @@ class ChangeRecorder private(observeeDbName: String, recordDbName: String,
   }
 
   def setUp() = {
+    val tableNameAndPrimaryColumnsList = using(DB(observeePool.borrow)) { observeeDb =>
+      observeeDb.autoClose(false)
+      for (tableName <- observeeDb.getTableNames())
+        yield (tableName, observeeDb.getTable(tableName).get.columns.filter(_.isPrimaryKey))
+    }
+
+    val queries = tableNameAndPrimaryColumnsList.map { case (tableName, primaryColumns) =>
+      val primaryColumnNameList = primaryColumns.map(_.name)
+      val primaryColumnNames = primaryColumnNameList.mkString(", ")
+
+      val queryForRecord =
+        s"CREATE TABLE IF NOT EXISTS $tableName (PRIMARY KEY($primaryColumnNames)) AS " +
+          s"SELECT $primaryColumnNames FROM $observeeDbName.$tableName WHERE FALSE"
+
+      // OLD.とNEW.はtrigger文固有のキーワード
+      // https://dev.mysql.com/doc/refman/5.6/ja/trigger-syntax.html
+      val queriesForObservee = Seq(
+        s"""CREATE TRIGGER changerecorder_observee_${tableName}_insert AFTER INSERT
+           |ON $observeeDbName.$tableName FOR EACH ROW
+           |REPLACE INTO $recordDbName.$tableName($primaryColumnNames)
+           |VALUES(${primaryColumnNameList.map("NEW." + _).mkString(", ")})""",
+        s"""CREATE TRIGGER changerecorder_observee_${tableName}_update AFTER UPDATE
+           |ON $observeeDbName.$tableName FOR EACH ROW
+           |REPLACE INTO $recordDbName.$tableName($primaryColumnNames)
+           |VALUES
+           |(${primaryColumnNameList.map("OLD." + _).mkString(", ")}),
+           |(${primaryColumnNameList.map("NEW." + _).mkString(", ")})""",
+        s"""CREATE TRIGGER changerecorder_observee_${tableName}_delete AFTER DELETE
+           |ON $observeeDbName.$tableName FOR EACH ROW
+           |REPLACE INTO $recordDbName.$tableName($primaryColumnNames)
+           |VALUES(${primaryColumnNameList.map("OLD." + _).mkString(", ")})"""
+      ).map(_.stripMargin.replace('\n', ' '))
+
+      (queriesForObservee, queryForRecord)
+    }
+
+    val temp = queries.unzip
+    val queriesForObservee = temp._1.flatten
+    val queriesForRerord = temp._2
+
     using(DB(observeePool.borrow)) { observeeDb =>
-      observeeDb.autoClose(false) // loanパターン使うときはautoCloseは自動でOFFにしてくれないものか…
-      using(DB(recordPool.borrow)) { recordDb =>
-        recordDb.autoClose(false) // loanパターン使うときはautoCloseは自動でOFFにしてくれないものか…
-        recordDb.autoCommit { implicit session =>
-          for (tableName <- observeeDb.getTableNames()) {
-            val primaryColumnNameList = observeeDb.getTable(tableName).get.columns.
-              withFilter(_.isPrimaryKey).map(_.name)
-            val primaryColumnNames = primaryColumnNameList.mkString(", ")
-
-            SQL( // 監視対象のテーブルと全く同じカラム定義になるようCREATE TABLE ... AS SELECT文を使う
-              s"CREATE TABLE IF NOT EXISTS $tableName (PRIMARY KEY($primaryColumnNames)) AS " +
-              s"SELECT $primaryColumnNames FROM $observeeDbName.$tableName WHERE FALSE"
-            ).execute.apply()
-
-            observeeDb.autoCommit { implicit session =>
-              // OLD.とNEW.はtrigger文固有のキーワード
-              // https://dev.mysql.com/doc/refman/5.6/ja/trigger-syntax.html
-              val queries = Seq(
-                s"""CREATE TRIGGER changerecorder_observee_${tableName}_insert AFTER INSERT
-                    |ON $observeeDbName.$tableName FOR EACH ROW
-                    |REPLACE INTO $recordDbName.$tableName($primaryColumnNames)
-                    |VALUES(${primaryColumnNameList.map("NEW." + _).mkString(", ")})""",
-                s"""CREATE TRIGGER changerecorder_observee_${tableName}_update AFTER UPDATE
-                    |ON $observeeDbName.$tableName FOR EACH ROW
-                    |REPLACE INTO $recordDbName.$tableName($primaryColumnNames)
-                    |VALUES
-                    |(${primaryColumnNameList.map("OLD." + _).mkString(", ")}),
-                    |(${primaryColumnNameList.map("NEW." + _).mkString(", ")})""",
-                s"""CREATE TRIGGER changerecorder_observee_${tableName}_delete AFTER DELETE
-                    |ON $observeeDbName.$tableName FOR EACH ROW
-                    |REPLACE INTO $recordDbName.$tableName($primaryColumnNames)
-                    |VALUES(${primaryColumnNameList.map("OLD." + _).mkString(", ")})"""
-              )
-              queries.map(_.stripMargin.replace('\n', ' ')).foreach(SQL(_).execute.apply())
-            }
-          }
-        }
+      observeeDb.autoCommit { implicit session =>
+        queriesForObservee.foreach(SQL(_).execute.apply())
       }
     }
+
+    using(DB(recordPool.borrow)) { recordDb =>
+      recordDb.autoCommit { implicit session =>
+        queriesForRerord.foreach(SQL(_).execute.apply())
+      }
+    }
+
   }
 
   def tearDown() = {
-    using(DB(observeePool.borrow)) { observeeDb =>
-      observeeDb.autoCommit { implicit session =>
+    val queries = using(DB(observeePool.borrow)) { observeeDb =>
+      observeeDb.readOnly { implicit session =>
         val q = "SELECT TRIGGER_NAME FROM information_schema.triggers " +
           s"WHERE TRIGGER_SCHEMA = '$observeeDbName'"
         val triggerNames = SQL(q).map(_.string(1)).list.apply()
         for (triggerName <- triggerNames)
-          SQL(s"DROP TRIGGER $observeeDbName.$triggerName").execute.apply()
+          yield s"DROP TRIGGER $observeeDbName.$triggerName"
+      }
+    }
+
+    using(DB(observeePool.borrow)) { observeeDb =>
+      observeeDb.autoCommit { implicit session =>
+        queries.foreach(SQL(_).execute.apply())
       }
     }
   }
